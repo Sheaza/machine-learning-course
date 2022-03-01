20bf8ddbdbda62528f155e029245849b2dc07ac9
s464980 <natwol6@st.amu.edu.pl>
2022-03-01 21:48:29 +0100

    first section done

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -5,6 +5,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 dataset = pd.read_csv("data/Data.csv")
@@ -21,3 +23,9 @@ x = np.array(ct.fit_transform(x))
 
 le = LabelEncoder()
 y = le.fit_transform(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=1)
+
+sc = StandardScaler()
+x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])
