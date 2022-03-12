9451ba83ae9347a7c65f1ba69d9445315a934155
Natalia <nataliawolowiec3@gmail.com>
2022-03-12 23:02:47 +0100

    encoded categorical feature

--- multiple-linear-regression.py
+++ multiple-linear-regression.py
@@ -3,7 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
 
 dataset = pd.read_csv("data/50_Startups.csv.csv")
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+
