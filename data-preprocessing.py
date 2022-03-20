4d198e9d4acaf646207bceba20a9749a38a3872c
Natalia <nataliawolowiec3@gmail.com>
2022-03-20 14:37:35 +0100

    reorganised files

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
@@ -9,7 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-dataset = pd.read_csv("data/Data.csv")
+dataset = pd.read_csv("regression/data/Data.csv")
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
@@ -24,7 +23,7 @@ x = np.array(ct.fit_transform(x))
 le = LabelEncoder()
 y = le.fit_transform(y)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
 
 sc = StandardScaler()
 x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
