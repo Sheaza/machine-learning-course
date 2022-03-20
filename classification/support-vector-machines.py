922d6f6cdc9c16c63c56f84aa9b964e0da15e51f
Natalia <nataliawolowiec3@gmail.com>
2022-03-20 21:33:31 +0100

    done svc

---
+++ classification/support-vector-machines.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv('data/Social_Network_Ads.csv')
+
+x = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+
+sc = StandardScaler()
+x_test = sc.fit_transform(x_test)
+x_train = sc.transform(x_train)
+
+svc = SVC(kernel='linear', random_state=0)
+svc.fit(x_train, y_train)
+
+y_pred = svc.predict(x_test)
+print(svc.predict(sc.transform([[30, 87000]])))
+print(np.concatenate((y_test.reshape(len(y_test), 1), y_pred.reshape(len(x_test), 1)), axis=1))
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+print(conf_matrix)
+
+acc_score = accuracy_score(y_test, y_pred)
+print(acc_score)
