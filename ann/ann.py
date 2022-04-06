bbc8b12e48228676e38c44e9eea65949fe9c4783
Sheaza <nataliawolowiec3@gmail.com>
2022-04-06 20:51:16 +0200

    done ann

---
+++ ann/ann.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv('data/Churn_Modelling.csv')
+
+X = data.iloc[:, 3:-1].values
+y = data.iloc[:, -1].values
+
+# categorical data encoding
+
+le = LabelEncoder
+X[:, 2] = le.fit_transform(X[:, 2])
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+# splitting before scaling for neural networks we always HAVE TO scale features
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
+sc = StandardScaler
+
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# building ann
+
+ann = tf.keras.models.Sequential()
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+ann.fit(X_train, y_train, batch_size=32, epochs=100)
+
+# single prediction
+
+print(ann.predict(sc.transform([[1.0, 0.0, 0.0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])) > 0.5)
+
+# predictions
+
+y_pred = ann.predict(X_test)
+y_pred = y_pred > 0.5
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
